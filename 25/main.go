e52ffb7c4f96ad9a3e71764fc5f1e1a936052593
Denis <116588516+dreamcoiI@users.noreply.github.com>
2023-10-02 21:55:23 +0300

    update task

--- 25/main.go
+++ 25/main.go
@@ -5,13 +5,18 @@ import (
 	"time"
 )
 
-func Sleep(seconds int) {
-	duration := time.Duration(seconds) * time.Second
-	time.Sleep(duration)
+func Sleep(duration time.Duration) {
+
+	<-time.After(duration * time.Second)
+
 }
 
 func main() {
-	fmt.Println("Start")
-	Sleep(4)
+
+	fmt.Println("START")
+
+	Sleep(2)
+
 	fmt.Println("END")
+
 }
